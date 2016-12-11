9220cb0dfb6618e89b6b4d0ca5ba36011dab40a4
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2016-12-11 16:43:50 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  enable :sessions
+  set :session_secret, "secret"
+   get '/' do
+     erb :index
+   end
+   post '/checkout' do
+     session[:item] = params[:item]
+     @session = session
+     erb :card_items
+   end
 end
